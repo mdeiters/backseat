d8ea1b76588f59751077f931094e1fcb7217d254
matthew deiters <mdeiters@gmail.com>
2009-03-02 17:38:41 -0600

    fixing

--- config/environment.rb
+++ config/environment.rb
@@ -8,12 +8,14 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.gem 'haml', :version => '2.0.8'
   config.gem 'fastercsv'
+  config.gem 'ruby-openid', :lib => 'openid', :version => '>=2.0.4'
 
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
   config.time_zone = 'UTC'
 
+  # config.plugins = ['open_id_authentication', 'jrails', 'haml']
   config.action_controller.session = {
     :session_key => '_backseat_session',
     :secret      => 'ede201e848d759c21e2701838fe4165056b9c9145ab531b80ed19fde750f4243cc01742c9485c3601e500e733948ec089b35093b2f9deb2803d7b0cf3b3b42f2'
